7287f27e4e7ad2848dc05b16000eecf2abbbb471
최지훈 <hunnx27@i-screamkids.com>
2022-05-24 17:40:18 +0900

    feat: 로그인페이지 기본 레이아웃
    - 로그인페이지 구현중

--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -1,20 +1,43 @@
 <template>
   <v-container fluid>
-    <h3>함께 만드는 유아교사 필수앱</h3>
-    <h1>원앤집</h1>
-    <input type="text" name="" id="" placeholder="이메일 주소"><br/>
-    <input type="text" name="" id="" placeholder="비밀번호"><br/>
-    <button>LOGIN</button>
-    <hr>
-    <button>카카오톡</button>
-    <button>페이스북</button>
-    <button>구글</button>
+    <v-row>
+      <v-col cols="12" sm="6" md="12">
+        <h3>함께 만드는 유아교사 필수앱</h3>
+      </v-col>
+      <v-col cols="12" sm="6" md="12">
+        <h1>원앤집</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6" md="12">
+        <v-text-field v-model="user.name" :rules="inputRules" label="이메일 주소" placeholder="Placeholder" outlined></v-text-field>
+      </v-col>
+      <v-col cols="12" sm="6" md="12">
+        <v-text-field v-model="user.password" :rules="inputRules" label="비밀번호" outlined></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6" md="12">
+        <v-btn block color="primary" @click="login">LOGIN</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" sm="6" md="12">
+      <ul>
+        <li v-for="social in socials" v-bind:key="social.socialType">
+          <button @click="socialLogin(social.socialType)"><img :src="social.src"/></button>
+        </li>
+      </ul>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 /* eslint-disable */
 import CounterItem from '/src/components/moduleB/PreviewItem.vue'
+import $ from '@/util/utils'
+
 export default {
   name: 'LoginItem',
   props: {
@@ -22,6 +45,54 @@ export default {
   },
   components:{
     CounterItem
+  },
+  data:()=>({
+    user:{
+      name: '',
+      password: ''
+    },
+    inputRules:[
+      value => !!value || 'Required.',
+      value => (value && value.length >= 3) || 'Min 3 characters'
+    ],
+    socials: [
+        {
+          socialType: 'google',
+          src: $.getSocialImage('google'),
+          width: '32px',
+          height: '32px',
+          comment: '구글 로그인'
+        },
+        {
+          socialType: 'facebook',
+          src: $.getSocialImage('facebook'),
+          width: '32px',
+          height: '32px',
+          comment: '페이스북 로그인'
+        },
+        {
+          socialType: 'naver',
+          src: $.getSocialImage('naver'),
+          width: '32px',
+          height: '32px',
+          comment: '네이버 로그인'
+        },
+        {
+          socialType: 'kakao',
+          src: $.getSocialImage('kakao'),
+          width: '32px',
+          height: '32px',
+          comment: '카카오 로그인'
+        }
+      ]
+  }),
+  methods:{
+    login(){
+      alert(this.user.name);
+    },
+    socialLogin(src){
+      alert(src)
+    }
   }
 }
 </script>
