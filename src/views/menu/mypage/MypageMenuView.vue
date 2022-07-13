64473fad31c35860254d72e5f52b1a8eeb1226b2
parkyongchan <yongchan@i-screamkids.com>
2022-07-13 13:48:16 +0900

    내 정보 링크 버튼 스타일 추가

--- src/views/menu/mypage/MypageMenuView.vue
+++ src/views/menu/mypage/MypageMenuView.vue
@@ -1,9 +1,12 @@
 <template>
   <!-- Wrap START -->
   <div class="mypageMenuView">
-
     <div>
-      <router-link to="/menu/mypage/myinfo">
+      <router-link
+        to="/menu/mypage/myinfo"
+        class="v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default"
+        style="width: 100%"
+      >
         내 정보
         <span>
           <v-btn icon><v-icon>mdi-chevron-right</v-icon></v-btn>
@@ -11,35 +14,32 @@
       </router-link>
     </div>
     <div>
-      <router-link to="/menu/mypage/pointHistory">
+      <router-link
+        to="/menu/mypage/pointHistory"
+        class="v-btn v-btn--is-elevated v-btn--has-bg theme--light v-size--default"
+        style="width: 100%; margin-top: 30px"
+      >
         포인트 내역
         <v-btn icon><v-icon>mdi-chevron-right</v-icon></v-btn>
       </router-link>
     </div>
-    
   </div>
   <!-- Wrap END -->
-
 </template>
 
-<style>
-
-</style>
 <script>
-
 export default {
-  name: 'MypageView',
-  components:{
-    
+  name: "MypageView",
+  components: {},
+  data() {
+    return {};
   },
-  data(){
-    return{
-      
-    }
-  },
-  methods:{
-    
-  }
-
-}
+  methods: {},
+};
 </script>
+
+<style lang="scss" scoped>
+.mypageMenuView {
+  padding: 30px 15px 15px;
+}
+</style>
