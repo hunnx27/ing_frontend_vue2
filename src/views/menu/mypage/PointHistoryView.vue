867224da30692bf7547e1893dc44d64b3b36d685
parkyongchan <yongchan@i-screamkids.com>
2022-07-13 18:02:19 +0900

    레이아웃 수정작업

--- src/views/menu/mypage/PointHistoryView.vue
+++ src/views/menu/mypage/PointHistoryView.vue
@@ -1,10 +1,16 @@
 <template>
   <!-- Wrap START -->
   <div class="pointHistory page-wrap">
-    현재 포인트 3050p
-    <div>오늘 / -100 / 상담질문등록</div>
-    <div>2022.05.16 / +50 / 로그인-출석</div>
-    <div>2022.05.16 / +3000 / 회원가입</div>
+    현재 포인트 <span class="point_num">3050p</span>
+    <div class="pointbox">
+      <span>오늘</span> <span>-100</span><span>상담질문등록</span>
+    </div>
+    <div class="pointbox">
+      <span>2022.05.16</span><span>+50</span><span>로그인-출석</span>
+    </div>
+    <div class="pointbox">
+      <span>2022.05.16</span><span>+3000</span><span>회원가입</span>
+    </div>
   </div>
   <!-- Wrap END -->
 </template>
@@ -29,4 +35,28 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.point_num {
+  color: #8b00fd;
+}
+.pointbox {
+  display: flex;
+  flex-wrap: wrap;
+  height: 40px;
+  margin-top: 20px;
+  padding: 0 20px;
+  background: #fff;
+  border: 1px solid #cecece;
+  box-sizing: border-box;
+  border-radius: 10px;
+  box-shadow: 1px 2px 4px -3px;
+  line-height: 40px;
+
+  span {
+    display: inline-block;
+    width: calc(100% / 3);
+    font-size: 15px;
+    text-align: center;
+  }
+}
+</style>
