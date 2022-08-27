cabdc04296ac5fc7f87230160a6d058d102cb4df
hunnx27 <hunnx27@gmail.com>
2022-08-27 17:38:47 +0900

    feat: 통합리뷰관련 API정리 추가

--- src/api/review.js
+++ src/api/review.js
@@ -36,6 +36,7 @@ export default {
     const URI = `${ROOT_URI.REVIEW}/amt?page=${page}&size=${size}&sort=${sort}`
     req.get(URI, success, fail);
   },
+
   saveCompany(form, success, fail){
     const URI = `${ROOT_URI.COMPANY}`
     req.postMultipart(URI, form, success, fail);
@@ -48,4 +49,38 @@ export default {
     const URI = `${ROOT_URI.YEARAMT}`
     req.post(URI, body, success, fail);
   },
+
+
+// 컴퍼니 정보가져오기
+// /api/company/{id}
+
+// 단건조회
+// /api/reviews/company/{id}
+// /api/reviews/interview/{id}
+// /api/reviews/amt/{id}
+
+// 기관의 리스트가져오기
+// /api/reviews/company/companies/{companyId}
+// /api/reviews/interview/companies/{companyId}
+// /api/reviews/amt/companies/{companyId}
+  getCompanyInfo(id, success, fail){
+    
+    const URI = `${ROOT_URI.COMPANY}/${id}`
+    req.get(URI, success, fail);
+  },
+  getCompanyReviewListById(){
+
+  },
+  getInterviewReviewListById(){
+
+  },
+  getYearamtReviewListById(){
+
+  },
+  getCompanyJipyoById(){
+
+  },
+  getCompanyDetailInfo(){
+
+  }
 }
